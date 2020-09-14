12d6768075738e6fcb7519962cfe33469e64af89
Phạm Minh Tiến <phaminhtien2424@gmail.com>
2020-09-14 11:38:29 +0700

    (feat): update

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,40 +1,109 @@
-import React, { useContext, useState } from 'react'
-import ContactContext from '../../context/contact/contactContext'
-const ContactForm = () => {
-    const contactContext = useContext(ContactContext);
+import React, { useState, useContext, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
-    const [contact, setContact] = useState({
+const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { addContact, updateContact, clearCurrent, current } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
         name: '',
         email: '',
         phone: '',
-        type: 'personal'
-    });
-    const { name, email, phone, type } = contact
-
-    const onChange = event => setContact({ ...contact, [event.target.name]: event.target.value })
-    const onSubmit = event => {
-        event.prevenDefault();
-        contactContext.addContact(contact);
-        setContact({
-            name: '',
-            email: '',
-            phone: '',
-            type: 'personal'
-        });
+        type: 'personal',
+      });
     }
-    return (
-        <form onSubmit={onSubmit}>
-            <h2 className="text-primary">Add Contact</h2>
-            <input type="text" name="name" placeholder="Name" value={name} onChange={onChange} />
-            <input type="text" name="email" placeholder="Email" value={email} onChange={onChange} />
-            <input type="text" name="phone" placeholder="Phone" value={phone} onChange={onChange} />
-            <h5>Contact Type</h5>
-            <input type="radio" name="type" value="personal" checked={type === 'personal'} onChange={onChange} /> Personal{' '}
-            <input type="radio" name="type" value="professional" checked={type === 'professional'} onChange={onChange} /> Professional
-            <div>
-                <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
-            </div>
-        </form>
-    )
-}
-export default ContactForm
+  }, [contactContext, current]);
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal',
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
+  };
+
+  const clearAll = () => {
+    clearCurrent();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2 className='text-primary'>
+        {current ? 'Edit Contact' : 'Add Contact'}
+      </h2>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        name='email'
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type='text'
+        placeholder='Phone'
+        name='phone'
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact Type</h5>
+      <input
+        type='radio'
+        name='type'
+        value='personal'
+        checked={type === 'personal'}
+        onChange={onChange}
+      />{' '}
+      Personal{' '}
+      <input
+        type='radio'
+        name='type'
+        value='professional'
+        checked={type === 'professional'}
+        onChange={onChange}
+      />{' '}
+      Professional
+      <div>
+        <input
+          type='submit'
+          value={current ? 'Update Contact' : 'Add Contact'}
+          className='btn btn-primary btn-block'
+        />
+      </div>
+      {current && (
+        <div>
+          <button className='btn btn-light btn-block' onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default ContactForm;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -3,52 +3,78 @@ import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
-    ADD_CONTACT,
-    DELETE_CONTACT,
-    SET_CURRENT,
-    CLEAR_CURRENT,
-    UPDATE_CONTACT,
-    FILTER_CONTACTS,
-    CLEAR_FILTER,
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
 } from '../types';
 
 const ContactState = (props) => {
-    const initialState = {
-        contacts: [
-            {
-                id: 1,
-                name: 'Jill Johnson',
-                email: 'jill@gmail.com',
-                phone: '111-111-1111',
-                type: 'personal',
-            },
-            {
-                id: 2,
-                name: 'Sara Watson',
-                email: 'sara@gmail.com',
-                phone: '222-222-2222',
-                type: 'professional',
-            },
-        ],
-    };
-    const [state, dispatch] = useReducer(contactReducer, initialState);
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-2222',
+        type: 'professional',
+      },
+    ],
+    current: null,
+  };
 
-    // add contact
-    const addContact = (contact) => {
-        contact.id = uuid.v4();
-        dispatch({ type: ADD_CONTACT, payload: contact });
-    };
-    // delete contact
-    // set current contact
-    // clear current contact
-    // update contact
-    // filter contacts
-    // clear filter
+  const [state, dispatch] = useReducer(contactReducer, initialState);
 
-    return (
-        <ContactContext.Provider value={{ contacts: state.contacts, addContact }} >
-            {props.children}
-        </ContactContext.Provider>
-    );
+  // add contact
+  const addContact = (contact) => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+  // delete contact
+
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+  // set current contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+  // clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  // update contact
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+  // filter contacts
+  // clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
 };
 export default ContactState;
